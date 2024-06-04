49f12cd6ebf258d87acbbe6559f7e0cfdd7e9c56
Ash <Ash@LAPTOP-S5AQL09G>
2024-06-04 20:28:03 +0530

    first commit

---
+++ Jenkins/src/main/java/jenkinsprogram/JenkinsProgram.java
@@ -0,0 +1,13 @@
+package jenkinsprogram;
+
+public class JenkinsProgram {
+
+	public static void main(String[] args) {
+		String name="Ash";
+		String location="Tvm";
+		System.out.println("Name is "+name);
+		System.out.println("Location is "+location);
+
+	}
+
+}
